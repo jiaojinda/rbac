51c9ff498e14bba801b62410bd10936ec244f777
jiaojinda <jiaojnda@163.com>
2018-11-22 00:32:35 +0800

    用户模块修改为RESTful

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser');
 var url = require('url');
 var md5 =require("md5");
 var DbUtil = require('../dbUtil');
-var router = express.Router();
 var app = express();
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -13,7 +12,7 @@ var roleCollection = "role";
 var userRoleCollection = "user-role";
 
 //列表页
-router.get('/list', function (req, res) {
+app.get('/', function (req, res) {
     dbUtil.find(userCollection,{},function (result) {
         res.render('user/list', { list: result,userId:req.session.user.userId});
     });
@@ -21,7 +20,7 @@ router.get('/list', function (req, res) {
 
 
 //账号唯一校验
-router.post('/checkAccount', urlencodedParser, function (req, res) {
+app.post('/checkAccount', urlencodedParser, function (req, res) {
     var whereJson = {account:req.body.account};   // 查询条件
     dbUtil.find(userCollection,whereJson,function (result) {
         res.json({"flag":result.length>0});
@@ -29,7 +28,7 @@ router.post('/checkAccount', urlencodedParser, function (req, res) {
 })
 
 //添加页
-router.get('/add', function(req, res) {
+app.get('/add', function(req, res) {
     app.use(express.static('public'));
     dbUtil.find(roleCollection,{},function (result) {
         res.render('user/add', { list: result});
@@ -37,7 +36,7 @@ router.get('/add', function(req, res) {
 });
 
 //添加
-router.post('/add', urlencodedParser, function (req, res) {
+app.post('/', urlencodedParser, function (req, res) {
     var password = md5(req.body.password+"salt");
     var userJson ={
         account:req.body.account,
@@ -46,31 +45,34 @@ router.post('/add', urlencodedParser, function (req, res) {
         sex:req.body.sex
     };
     dbUtil.insertOne(userCollection,userJson,function (result) {
-        var userId = result.ops[0]._id;
-        var roleArr=req.body.roleStr.split(',');
-        if(roleArr.length>0){
-            var ObjectID = require('mongodb').ObjectID;
-            var roleJsonObj = [];
-            for(var i = 0;i<roleArr.length;i++){
-                var roleJson ={
-                    userId:userId,
-                    roleId:ObjectID(roleArr[i])
-                };
-                roleJsonObj.push(roleJson);
+        var roleStr = req.body.roleStr;
+        if(roleStr!=""){
+            var userId = result.ops[0]._id;
+            var roleArr=roleStr.split(',');
+            if(roleArr.length>0){
+                var ObjectID = require('mongodb').ObjectID;
+                var roleJsonObj = [];
+                for(var i = 0;i<roleArr.length;i++){
+                    var roleJson ={
+                        userId:userId,
+                        roleId:ObjectID(roleArr[i])
+                    };
+                    roleJsonObj.push(roleJson);
+                }
+                dbUtil.insertMany(userRoleCollection,roleJsonObj,function () {
+                    res.redirect(302, '/user');
+                });
             }
-            dbUtil.insertMany(userRoleCollection,roleJsonObj,function () {
-                res.redirect(302, '/user/list');
-            });
+        }else{
+            res.redirect(302, '/user');
         }
     });
 })
 
 //修改页
-router.get('/update', function(req, res) {
-    // 解析 url 参数
-    var params = url.parse(req.url, true).query;
+app.get('/:id', function(req, res) {
     var ObjectID = require('mongodb').ObjectID;
-    var userId = ObjectID(params.id);
+    var userId = ObjectID(req.param('id'));
     var whereJson = {_id:userId};   // 查询条件
     dbUtil.find(userCollection,whereJson,function (result) {
         var roleWhereJson = {userId:userId};
@@ -99,48 +101,51 @@ router.get('/update', function(req, res) {
 });
 
 //修改
-router.post('/update', urlencodedParser, function (req, res) {
+app.put('/:id', urlencodedParser, function (req, res) {
     var ObjectID = require('mongodb').ObjectID;
-    var userId = ObjectID(req.body.id);
+    var userId = ObjectID(req.param('id'));
     var whereJson = {_id:userId};
     var updateStr = {$set: {
             name:req.body.name,
             sex:req.body.sex
         }};
     dbUtil.updateOne(userCollection,whereJson,updateStr,function () {
-        var roleWhereJson = {userId:userId};
-        dbUtil.deleteMany(userRoleCollection,roleWhereJson,function () {
-            var roleArr=req.body.roleStr.split(',');
-            if(roleArr.length>0){
-                var roleJsonObj = [];
-                for(var i = 0;i<roleArr.length;i++){
-                    var roleJson ={
-                        userId:userId,
-                        roleId:ObjectID(roleArr[i])
-                    };
-                    roleJsonObj.push(roleJson);
+        var roleStr = req.body.roleStr;
+        if(roleStr!=""){
+            var roleWhereJson = {userId:userId};
+            dbUtil.deleteMany(userRoleCollection,roleWhereJson,function () {
+                var roleArr=roleStr.split(',');
+                if(roleArr.length>0){
+                    var roleJsonObj = [];
+                    for(var i = 0;i<roleArr.length;i++){
+                        var roleJson ={
+                            userId:userId,
+                            roleId:ObjectID(roleArr[i])
+                        };
+                        roleJsonObj.push(roleJson);
+                    }
+                    dbUtil.insertMany(userRoleCollection,roleJsonObj,function () {
+                        res.json({"ret":0});
+                    });
                 }
-                dbUtil.insertMany(userRoleCollection,roleJsonObj,function () {
-                    res.redirect(302, '/user/list');
-                });
-            }
-        });
+            });
+        }else{
+            res.json({"ret":0});
+        }
     });
 })
 
 //删除
-router.get('/del', function (req, res) {
-    // 解析 url 参数
-    var params = url.parse(req.url, true).query;
+app.delete('/:id', function (req, res) {
     var ObjectID = require('mongodb').ObjectID;
-    var userId = ObjectID(params.id)
+    var userId = ObjectID(req.param('id'))
     var whereJson = {_id:userId};  // 条件
     dbUtil.deleteOne(userCollection,whereJson,function () {
         var roleWhereJson = {userId:userId};
         dbUtil.deleteMany(userRoleCollection,roleWhereJson,function () {
-            res.redirect(302, '/user/list');
+            res.json({"ret":0});
         });
     });
 })
 
-module.exports = router;
+module.exports = app;
